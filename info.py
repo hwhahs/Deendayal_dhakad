5f792578f03bbc7ff0c3f8d7c279afd376f1305d
hwhahs <royal.spamzz@gmail.com>
2025-09-02 14:50:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -61,8 +61,8 @@ OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'contact us in the support group 👉
 # ============================
 # MongoDB Configuration
 # ============================
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://littlegirlrocksok:ztHtBEQbv9PYEYZZ@cluster0.poamxgy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://HTriggers:TZCVCTIIUTRGBB@cluster0.cmou7ff.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://cute61283_db_user:DmnZRN9dwdQPReFZ@cluster0.g6yzgrq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://cute61283_db_user:DmnZRN9dwdQPReFZ@cluster0.g6yzgrq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Deendayal_files')
 
